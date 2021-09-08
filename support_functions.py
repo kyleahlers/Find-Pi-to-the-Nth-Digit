f1740fbb11d52bc5267fee0f13ba07af07fb5608
Kyle Ahlers <82334392+kyleahlers@users.noreply.github.com>
2021-09-08 19:55:00 -0400

    Update supprort_functions.py
    - Added comments to clarify code

--- support_functions.py
+++ support_functions.py
@@ -1,10 +1,12 @@
-# converts int or float number to list for comparisons.
+# this function converts int or float number to list for comparisons.
 # ie. 123.40 to [1, 2, 3, 4, 0]
 
 def num_to_list(n):
     
+    # assign empty list to result variable
     result = []
     
+    # converts input (n) to a iterable string. Appends all integers to list
     for element in str(n):
         if element.isdigit() == True:
             result.append( int(element) )
@@ -14,11 +16,14 @@ def num_to_list(n):
 
 ##########################################################################
 # compares two lists of equivalent length up to n digits
+
 def compare_list(a,b):
     green_light = True
     compare_result = []
     element = 0
 
+    # compares two entire lists and appends all equivalent values. when element 
+    # is no longer equal it breaks out
     while element < len(a):
 
         if a[element] == b[element]:
@@ -32,6 +37,8 @@ def compare_list(a,b):
     
     
 ##########################################################################
+# converts list of numbers (validates as pi approximation) and converts 
+# it to a float number
 def pi_list_to_num(test):
       
     str_test = []
